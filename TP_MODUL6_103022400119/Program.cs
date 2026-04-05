c5cced43460eb1b6735f9553f9534d9ee6e1238b
Daniarthawiku <wikunugroho735@gmail.com>
2026-04-05 19:36:21 +0700

    menambahkan implementasi design by contract

--- TP_MODUL6_103022400119/Program.cs
+++ TP_MODUL6_103022400119/Program.cs
@@ -1,19 +1,48 @@
-﻿public class SayaMusikTrack
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+public class SayaMusikTrack
 {
-    private int id;
+    private int id = 9999;
     private string playCount;
     private string title;
+
 
     public SayaMusikTrack(string title)
     {
         this.id = id;
         this.title = title;
         this.playCount = "0";
+
+
+
+        Contract.Requires(!string.IsNullOrEmpty(title), "Title cannot be null or empty.");
+        Contract.Requires(title.Length <= 100, "Max title length is 100.");
     }
 
     void IncreasePlayCount(int count)
     {
-        this.playCount += count;
+        Debug.Assert(count >= 0 && count <= 10000000, "Input penambahan maksimal 10.000.000");
+        try
+        {
+            int currentCount = int.Parse(this.playCount);
+            checked
+            {
+                currentCount = currentCount + count;
+            }
+
+            this.playCount = currentCount.ToString();
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Terjadi overflow! Play count melebihi batas maksimum integer.");
+            throw;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 
     void PrintTrackInfo()
@@ -21,5 +50,38 @@
         Console.WriteLine($"ID: {id}");
         Console.WriteLine($"Title: {title}");
         Console.WriteLine($"Play Count: {playCount}");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusikTrack track = new SayaMusikTrack("Shape Of My Hearth");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nsePotipay");
+                    
+
+                    Console.WriteLine("Masukan PlayCount:");
+                    int playTest = Convert.ToInt32(Console.ReadLine());
+                    for (int j = 0; j < 300; j++)
+                    {
+                        track.IncreasePlayCount(playTest);
+                    }
+
+                    track.PrintTrackInfo();
+                }
+                catch (Exception ex)
+                {
+                    track.PrintTrackInfo();
+                    Console.WriteLine($"\nProgram stop karena: {ex.GetType().Name}");
+                    break;
+                }
+            } while (true);
+            
+            Console.WriteLine();
+        }
     }
 }
--- TP_MODUL6_103022400119/obj/Debug/net8.0/TP_MODUL6_103022400119.AssemblyInfo.cs
+++ TP_MODUL6_103022400119/obj/Debug/net8.0/TP_MODUL6_103022400119.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400119")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d077abd50a612d93804ee1cf6664e68a7af8384")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400119")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400119")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
