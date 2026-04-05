6d077abd50a612d93804ee1cf6664e68a7af8384
Daniarthawiku <wikunugroho735@gmail.com>
2026-04-05 18:49:38 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400119/Program.cs
+++ TP_MODUL6_103022400119/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaMusikTrack
+{
+    private int id;
+    private string playCount;
+    private string title;
+
+    public SayaMusikTrack(string title)
+    {
+        this.id = id;
+        this.title = title;
+        this.playCount = "0";
+    }
+
+    void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    void PrintTrackInfo()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
